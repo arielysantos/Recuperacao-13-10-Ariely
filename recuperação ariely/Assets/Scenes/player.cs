543c8b25ce82850f636dc8c44ee0f7460e150e20
arielysantos <arielys923@gmail.com>
2024-09-26 14:25:02 -0300

    Método HandleInput
    HandleInput

--- recuperação ariely/Assets/Scenes/player.cs
+++ recuperação ariely/Assets/Scenes/player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,5 +17,10 @@ public class SnakeController : MonoBehaviour
             moveTimer = 0;
             GameManager.Instance.MoveSnake();
         }
+    }
+
+    private void HandleInput()
+    {
+        throw new NotImplementedException();
     }
 }
