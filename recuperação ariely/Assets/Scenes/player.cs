bab7389c3413838caa242763800a67fcd90a6d46
arielysantos <arielys923@gmail.com>
2024-09-26 14:22:37 -0300

    Método SnakeController
    SnakeController

---
+++ recuperação ariely/Assets/Scenes/player.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    private float moveTimer;
+    public float moveInterval = 0.2f;
+}
