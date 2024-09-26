e7477727c60f501dc8267f6b3084241547e3b8b2
arielysantos <arielys923@gmail.com>
2024-09-26 14:52:42 -0300

    Método MoveSnake
     MoveSnake

--- recuperação ariely/Assets/Scenes/GameObject.cs
+++ recuperação ariely/Assets/Scenes/GameObject.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class GameObject : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; } 
+    public static GameManager Instance { get; private set; }
 
-    public GameObject snakeSegmentPrefab;  
-    public GameObject foodPrefab;  
+    public GameObject snakeSegmentPrefab;
+    public GameObject foodPrefab;
 
-    public int boardWidth = 20;    
-    public int boardHeight = 20;   
+    public int boardWidth = 20;
+    public int boardHeight = 20;
 
-    private List<Transform> snakeSegments = new List<Transform>(); 
-    private Vector2Int currentDirection;  
-    private Transform foodInstance;  
+    private List<Transform> snakeSegments = new List<Transform>();
+    private Vector2Int currentDirection;
+    private Transform foodInstance;
 
     private Vector2Int foodPosition;
 
@@ -23,8 +23,8 @@ public class GameObject : MonoBehaviour
     {
         if (Instance == null)
         {
-            Instance = this;  
-            InitializeGame();  
+            Instance = this;
+            InitializeGame();
         }
         else
         {
@@ -32,12 +32,12 @@ public class GameObject : MonoBehaviour
         }
     }
 
-    
+
     private void InitializeGame()
     {
-        currentDirection = Vector2Int.right; 
-        CreateSnake();  
-        SpawnFood();    
+        currentDirection = Vector2Int.right;
+        CreateSnake();
+        SpawnFood();
     }
 
     private void SpawnFood()
@@ -56,8 +56,8 @@ public class GameObject : MonoBehaviour
 
         for (int i = 0; i < 3; i++)
         {
-           
-            Vector2Int position = startPosition - new Vector2Int(i, 0);  
+
+            Vector2Int position = startPosition - new Vector2Int(i, 0);
             AddSnakeSegment(position);
         }
     }
@@ -66,4 +66,33 @@ public class GameObject : MonoBehaviour
     {
         throw new NotImplementedException();
     }
+
+    public void MoveSnake()
+    {
+
+        Vector2Int newPosition = Vector2Int.RoundToInt(snakeSegments[0].position) + currentDirection;
+
+        if (CheckCollision(newPosition))
+        {
+            Debug.Log("Game Over!");
+
+            return;
+        }
+
+
+        if (newPosition == foodPosition)
+        {
+            SpawnFood();
+        }
+        else
+        {
+
+            Destroy(snakeSegments[snakeSegments.Count - 1].gameObject);
+            snakeSegments.RemoveAt(snakeSegments.Count - 1);
+        }
+
+
+        AddSnakeSegment(newPosition);
+    }
+
 }
