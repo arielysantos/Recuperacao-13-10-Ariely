0ff27719521e9015568a294145e64c2cc07951c5
arielysantos <arielys923@gmail.com>
2024-09-26 13:57:29 -0300

    Método GameManager Instance
     GameManager Instance

---
+++ recuperação ariely/Assets/Scenes/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public int width = 20;
+    public int height = 20;
+    public GameObject snakeSegmentPrefab;
+    public GameObject foodPrefab;
+    private List<Transform> snake = new List<Transform>();
+    private Vector2 direction;
+    private Transform food;
+}
